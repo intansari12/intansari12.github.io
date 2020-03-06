e68dcdc597d4f781936f48aef63a6f72cbac1767
intansari12 <isari.intan@gmail.com>
2020-03-06 15:47:16 +0700

    deploy

---
+++ precache-manifest.e266dae4bacde0212c41e45717927a47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f4c2ad2102535dabb3f302a3b6cc6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0d0fccb7f7db41677ad",
+    "url": "/static/css/main.30d1401b.chunk.css"
+  },
+  {
+    "revision": "5a5aeceed8e9f9a4eb56",
+    "url": "/static/js/2.faef7496.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.faef7496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d0fccb7f7db41677ad",
+    "url": "/static/js/main.e66236c2.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
